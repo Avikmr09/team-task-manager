a17b999168df9e7b4404bfc59c14905a1150177f
Avinash kumar <avikmr158@gmail.com>
2026-05-03 15:20:42 +0530

    Please provide the file changes or a description of the modifications you made so I can generate the appropriate commit message for you.

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/dashboard', require('./routes/dashboardRoutes'));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all route to serve React app for non-API requests
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
